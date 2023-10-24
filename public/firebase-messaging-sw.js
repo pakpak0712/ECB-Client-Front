d8ceace72990165042b2d6a80b1240a0a56cfb07
egChoi92 <2013agal@gmail.com>
2023-10-24 15:07:12 +0900

    Feat: 로그인 API 호출할 때 푸시 알림 토큰 추가

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+self.addEventListener('install', function () {
+	console.log('FCM SW Install...');
+	self.skipWaiting();
+});
+
+self.addEventListener('activate', function () {
+	console.log('FCM SW Activate...');
+});
+
+self.addEventListener('push', function (e) {
+	//console.log('Push: ', e.data.json());
+	if (!e.data.json()) return;
+
+	const resultData = e.data.json().notification;
+	const notificationTitle = resultData.title;
+	const notificationOptions = { body: resultData.body };
+	console.log('Push: ', { resultData, notificationTitle, notificationOptions });
+
+	self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function (e) {
+	console.log('Notification Click');
+
+	e.notification.close();
+	//e.waitUntil(clients.openWindow('/'));
+});
